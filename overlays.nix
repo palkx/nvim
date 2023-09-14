78d13aca6149bcaf4ad8d1cf965ae171d5841fa0
Mykhailo Nikiforov <u@xaked.com>
2023-09-14 21:03:17 +0300

    Init lazyvim

--- overlays.nix
+++
@@ -1,8 +0,0 @@
-{inputs, ...}: {
-  overlays = [
-    (final: prev: {
-      inherit (inputs.groovyls.packages.${final.system}) groovyls;
-      inherit (inputs.npm-groovy-lint.packages.${final.system}) npm-groovy-lint;
-    })
-  ];
-}
