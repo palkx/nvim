cf93c42ffca996ffee772a732721f3215f1756cc
Mykhailo Nikiforov <u@xaked.com>
2023-10-10 13:44:27 +0300

    [#11]: Disable mdformat (#12)
    fixes #11
    
    Reviewed-on: https://xaked.com/xaked/nvim/pulls/12
    Co-authored-by: Mykhailo Nikiforov <u@xaked.com>
    Co-committed-by: Mykhailo Nikiforov <u@xaked.com>

--- nvim/lua/plugins/lang/markdown.lua
+++ nvim/lua/plugins/lang/markdown.lua
@@ -4,31 +4,33 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
-        -- See https://xaked.com/xaked/nvim/issues/9
-        -- "marksman",
-        "mdformat",
+        "marksman",
+        -- TODO: remove when fixed
+        -- See https://xaked.com/xaked/nvim/issues/11
+        -- "mdformat",
       })
     end,
   },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      vim.list_extend(opts.sources, {
-        nls.builtins.formatting.mdformat,
-      })
-    end,
-  },
-  -- See https://xaked.com/xaked/nvim/issues/9
+  -- TODO: remove when fixed
+  -- See https://xaked.com/xaked/nvim/issues/11
   -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     servers = {
-  --       marksman = {},
-  --     },
-  --   },
+  --   "nvimtools/none-ls.nvim",
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     opts.sources = opts.sources or {}
+  --     vim.list_extend(opts.sources, {
+  --       nls.builtins.formatting.mdformat,
+  --     })
+  --   end,
   -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {},
+      },
+    },
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
