209f4d3ddd4faa92a3d3cd150bc2812fc64e4cea
Mykhailo Nikiforov <mn@palkoi.net>
2024-05-27 12:03:40 +0300

    feat(telescope): follow symlinks

--- config/src/nvim/lua/plugins/telescope.lua
+++ config/src/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,31 @@
 return {
-  "telescope.nvim",
+  "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
     build = "make",
     config = function()
       require("telescope").load_extension("fzf")
     end,
+  },
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        ".git/",
+        "^node_modules/",
+        ".cache",
+      },
+    },
+    pickers = {
+      live_grep = {
+        additional_args = { "-L" },
+      },
+      grep_string = {
+        additional_args = { "-L" },
+      },
+      find_files = {
+        hidden = true,
+        follow = true,
+      },
+    },
   },
 }
