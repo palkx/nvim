ad8dc7546bbc532fd6d66f46e8d60751abbc796e
Mykhailo Nikiforov <mn@palkoi.net>
2024-11-25 21:54:10 +0200

    feat: update lazyvim configuration

--- config/src/nvim/lua/config/lazy.lua
+++ config/src/nvim/lua/config/lazy.lua
@@ -33,13 +33,11 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "plugins.catppuccin" },
-    -- { import = "plugins.tokyonight" },
-    -- { import = "plugins.lang.nix" },
     { import = "plugins.lang.shell" },
-    { import = "plugins.misc.bigfile" },
     { import = "plugins.misc.tmux-navigator" },
   },
   defaults = {
@@ -48,28 +46,14 @@ require("lazy").setup({
     lazy = false,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
+    version = nil, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  news = {
-    lazyvim = false,
-    neovim = false,
-  },
   install = { colorscheme = { "catppuccin-macchiato" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, frequency = 60 * 60 * 24, }, -- automatically check for plugin updates once per day
   performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+    cache = {
+      enabled = true,
     },
   },
 })
