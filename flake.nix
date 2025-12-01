2ebdf4e6a3732b4f9f9cc6d95c255823c2f6640f
Mykhailo Nikiforov <mn@palkoi.net>
2025-12-01 21:19:28 +0200

    feat: update to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Neovim configuration, declaratively written using nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim.url = "github:nix-community/nixvim/nixos-25.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
