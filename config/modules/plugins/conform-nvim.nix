42719a9f92decc1780dedd6f7aba04ecbd00a5ec
Mykhailo Nikiforov <mn@palkoi.net>
2025-12-29 12:05:57 +0200

    feat(conform): do not disable formatters on timeout

--- config/modules/plugins/conform-nvim.nix
+++ config/modules/plugins/conform-nvim.nix
@@ -124,14 +124,15 @@
                 return
               end
 
-              if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-              end
+              -- if slow_format_filetypes[vim.bo[bufnr].filetype] then
+              --   return
+              -- end
 
               local function on_format(err)
-                if err and err:match("timeout$") then
-                  slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                end
+                return
+                -- if err and err:match("timeout$") then
+                --   slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                -- end
               end
 
               return { timeout_ms = 200, lsp_fallback = true }, on_format
